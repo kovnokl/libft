291ee89a946686086cf43afcbb082a52543deb37
Kevin Nickel <kevinnickel2003@gmail.com>
2022-12-02 15:23:32 +0100

    fixed for war machine

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: knickel <knickel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 22:12:56 by knickel           #+#    #+#             */
-/*   Updated: 2022/10/17 22:15:31 by knickel          ###   ########.fr       */
+/*   Updated: 2022/12/01 20:40:43 by knickel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,21 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
-
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		n *= -1;
+		ft_putchar_fd('-', fd);
+	}
+	if (n < 10)
+		ft_putchar_fd('0' + n, fd);
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd('0' + n % 10, fd);
+	}
 }
--- ft_split.c
+++ ft_split.c
@@ -5,32 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: knickel <knickel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/15 23:27:48 by knickel           #+#    #+#             */
-/*   Updated: 2022/10/27 22:00:30 by knickel          ###   ########.fr       */
+/*   Created: 2022/11/02 19:13:14 by knickel​​​​       #+#    #+#             */
+/*   Updated: 2022/12/02 15:21:53 by knickel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-size_t	count_substrs(char *s, char c);
-void	iterate_subs(char **substrings, size_t substr_cnt);
+size_t	count_substrs(const char *s, char c);
+void	iterate_subs(char **substrings, size_t sub_cnt, const char *s, char c);
+char	**empty_list(void);
+void	delete_list(char **substrings, size_t input_cnt);
 
 char	**ft_split(char const *s, char c)
 {
 	char	**substrings;
-	size_t	substr_cnt;
+	size_t	sub_cnt;
 
 	if (s == NULL)
-		return (NULL);
-	substr_cnt = count_substrs(s, c);
-	substrings = (char **)malloc(sizeof(char *) * (substr_cnt + 1));
+		return (empty_list());
+	sub_cnt = count_substrs(s, c);
+	substrings = (char **)malloc(sizeof(char *) * (sub_cnt + 1));
 	if (substrings == NULL)
 		return (NULL);
-	substrings[substr_cnt] = 0;
-	iterate_subs(substrings, substr_cnt);
+	substrings[sub_cnt] = NULL;
+	if (sub_cnt > 0)
+		iterate_subs(substrings, sub_cnt, s, c);
+	return (substrings);
 }
 
-size_t	count_substrs(char *s, char c)
+size_t	count_substrs(const char *s, char c)
 {
 	size_t	sub_amount;
 	size_t	str_len;
@@ -39,9 +44,9 @@ size_t	count_substrs(char *s, char c)
 	str_len = ft_strlen(s);
 	sub_amount = 0;
 	counter = 0;
-	if (str_len && s[0] && s[0] != c)
+	if (str_len != 0 && s[0] && s[0] != c)
 		sub_amount++;
-	while (counter < str_len - 1)
+	while (counter + 1 < str_len)
 	{
 		if (s[counter] == c && s[counter + 1] != c)
 			sub_amount++;
@@ -50,7 +55,53 @@ size_t	count_substrs(char *s, char c)
 	return (sub_amount);
 }
 
-void	iterate_subs(char **substrings, size_t substr_cnt)
+void	iterate_subs(char **substrings, size_t sub_cnt, const char *s, char c)
 {
-	
+	size_t	counter;
+	size_t	str_pos;
+	size_t	str_len;
+
+	counter = 0;
+	str_pos = 0;
+	while (s[str_pos] == c)
+		str_pos++;
+	while (counter < sub_cnt)
+	{
+		str_len = 0;
+		while (s[str_pos + str_len] != c && s[str_pos + str_len] != 0)
+			str_len++;
+		substrings[counter] = ft_substr(s, str_pos, str_len);
+		if (substrings[counter] == NULL)
+		{
+			delete_list(substrings, counter);
+			break ;
+		}
+		str_pos += str_len + 1;
+		while (counter + 1 < sub_cnt && s[str_pos] == c)
+			str_pos++;
+		counter++;
+	}
+}
+
+char	**empty_list(void)
+{
+	char	**empty_list;
+
+	empty_list = (char **)malloc(sizeof(char *));
+	empty_list[0] = NULL;
+	return (empty_list);
+}
+
+void	delete_list(char **substrings, size_t input_cnt)
+{
+	size_t	counter;
+
+	counter = 0;
+	while (counter < input_cnt)
+	{
+		free(substrings[counter]);
+		counter++;
+	}
+	free(substrings);
+	substrings = NULL;
 }
